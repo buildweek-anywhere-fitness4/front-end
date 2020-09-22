bcc09978a9c9016bc96a01c5d1d5a6689dfef952
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-09-22 19:23:16 -0400

    Yup and styling updated

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import RegisterForm from './Components/RegisterForm'
+import { Route, Link } from "react-router-dom"
 import './App.css';
-
 
 function App() {
   return (
     
     <div className="App">
-     
+      <Link to ="/RegisterForm"> Register Now </Link>
+      <Route path="/RegisterForm">
+        <RegisterForm />
+      </Route>
      
     </div>
   );
