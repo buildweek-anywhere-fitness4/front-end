9350bfc6c324decfade72fcc2b6c9d6a29dec989
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-09-22 21:14:24 -0400

    updated main files to delete features/loginForm branch

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,15 +1,56 @@
 import React from 'react';
-
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import RegisterForm from './Components/RegisterForm';
+import LoginForm from './Components/LoginForm';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//import styled from 'styled-components'
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+//   return (
     
-    <div className="App">
-   
+//     <div className="App">
+//       <Link to ="/RegisterForm"> Register Now </Link>
+//       <Route path="/RegisterForm">
+//         <RegisterForm />
+//       </Route>
+
      
-    </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/LoginForm"}>Anywhere Fitness</Link>
+          <div className="collapse navbar-collapse" id="#">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/LoginForm"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/RegisterForm"}>Register</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={LoginForm} />
+            <Route path="/LoginForm" component={LoginForm} />
+            <Route path="/RegisterForm" component={RegisterForm} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
