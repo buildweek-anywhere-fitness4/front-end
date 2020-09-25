c39e8cc8ca8018dbe96e0002ade2cae6112331e8
NotYourSyn <60495598+NotYourSyn@users.noreply.github.com>
2020-09-24 20:56:20 -0400

    updated yup to login form

--- anywhere-fitness/src/Components/LoginForm.js
+++ anywhere-fitness/src/Components/LoginForm.js
@@ -1,19 +1,66 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import * as Yup from 'yup'
 
-export default class Login extends Component {
-    render() {
+
+const loginFormSchema = Yup.object().shape({
+    email: Yup.string()
+              .email("Email is not in the expected format")
+              .required("Email is missing"),
+    password: Yup.string()
+              .required("Password is required")
+              .min(8, "Password must be 8 characters at minimum")
+  });
+  
+
+const Login = () => {
+    
+    const initialState = {
+        username: "",
+        password: ""
+    }
+    
+    const [formState, setFormState] = useState(initialState)
+    
+    const inputChangeHandler = (e) => {
+        setFormState({...formState, [e.target.name] : e.target.value})
+    }
+    
+    const loginSubmitHandler = (e) => {
+       
+        e.preventDefault();
+        console.log(formState);
+        axios.post("https://anywhere-fitness4.herokuapp.com/api/auth/client_login", formState)
+        .then(res => console.log(res))
+        .catch(err=> console.log(err))
+
+    };
+    
         return (
-            <form>
+            <form onSubmit={loginSubmitHandler}
+             validateSchema={loginFormSchema}>
+
                 <h3>Sign In</h3>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Username</label>
+                    <input 
+                    type="text" 
+                    className="form-control" 
+                    name="username" 
+                    value={formState.username} 
+                    onChange={inputChangeHandler} 
+                    placeholder="Enter username" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password"
+                     className="form-control" 
+                     name="password"
+                     value={formState.password}
+                     onChange={inputChangeHandler}
+                     placeholder="Enter password" />
                 </div>
 
                 <div className="form-group">
@@ -25,12 +72,12 @@ export default class Login extends Component {
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
                 <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
+                    <button>Forgot password?</button>
                 </p>
             </form>
         );
-    }
-}
-   
+    };
+    
+    export default Login;
 
 
